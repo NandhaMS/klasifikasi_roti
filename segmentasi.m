bbf6a372e6054e2f97604abd42420807261456ec
arrazy100 <afdhalarrazy111@gmail.com>
2020-11-22 08:34:47 +0700

    membuat script segmentasi dan klasifikasi

--- segmentasi.m
+++ segmentasi.m
@@ -1,43 +1,40 @@
-clc; close all;
+clc; clear; close all;
 image_folder = 'training_img'; % nama folder dataset gambar
 total_images = 15; % total gambar per kelas
 kelas = {'Burger', 'Croissants', 'Muffin', 'Pizza', 'RotiSobek', 'RotiTawar'}; % kelas klasifikasi jenis roti
-total_kelas = 1; % total kelas
+total_kelas = 6; % total kelas
 
 for i = 1:total_kelas % perulangan untuk i = 1 hingga 6 (total kelas)
     for j = 1:total_images % perulangan untuk j = 1 hingga 15 (total gambar)
         figure(i) % membuat figure untuk menampilkan gambar setiap kelas
         full_name = fullfile(strcat(image_folder, '\', kelas{i}), strcat(num2str(j),'.jpg')) ; % mengambil file j.jpg dari folder kelas index ke-i
         Img = imread(full_name); % baca gambar j.jpg yang telah diambil
-        RGB = segmentasi(Img, i);
-        gray = rgb2gray(RGB);
+        [gray, RGB] = segmentasi(Img);
         GLCM = graycomatrix(gray, 'Offset', [0 1; -1 1; -1 0; -1 -1]); % membuat matriks co-occurence tingkat abu-abu dari gambar n.gif
         stats = graycoprops(GLCM, {'contrast', 'correlation', 'energy', 'homogeneity'}); % menghitung statistik GLCM untuk mendapatkan nilai kontras, korelasi, energi, dan homogenitas
         ind = j + ((i - 1) * total_images);
-        CON(ind) = mean(stats.Contrast); % menghitung rata-rata dari matriks kontras, kemudian disimpan ke dalam matriks CON indeks ke-n
-        CORR(ind) = mean(stats.Correlation); % menghitung rata-rata dari matriks korelasi, kemudian disimpan ke dalam matriks CORR indeks ke-n
-        C{ind} = kelas{i};
-        %X{ind} = {CON, CORR, C}; % membuat matriks X, isi baris pertama dengan matriks CON dan baris kedua dengan matriks CORR, kemudian matriks X di transpose
+        CON = mean(stats.Contrast); % menghitung rata-rata dari matriks kontras, kemudian disimpan ke dalam matriks CON indeks ke-n
+        CORR = mean(stats.Correlation); % menghitung rata-rata dari matriks korelasi, kemudian disimpan ke dalam matriks CORR indeks ke-n
+        E = mean(stats.Energy);
+        H = mean(stats.Homogeneity);
+        stats = regionprops(gray, 'Perimeter', 'Area', 'Eccentricity'); % menghitung fitur bentuk gambar n.gif berdasarkan daerah pada gambar
+        P = stats.Perimeter; % menyimpan nilai perimeter
+        A = stats.Area; % menyimpan nilai area
+        Ec = stats.Eccentricity; % simpan eccentricity ke dalam matriks E indeks ke-n
+        M = 4*pi*A/P^2; % hitung nilai metric, simpan ke dalam matriks M index ke-n
+        C = kelas{i};
+        X{ind} = {CON CORR E Ec M H C};
         subplot(3,5,j), imshow(RGB); % menampilkan gambar j.jpg pada subplot ke-j
     end
 end
 
-X = table(
+writetable(cell2table(X'), 'fitur.csv', 'WriteVariableNames', 0);
 
-%xlswrite('X.xls', X');
-
-function RGB = segmentasi(I, kelas)
+function [gray, RGB] = segmentasi(I)
     HSV = rgb2hsv(I);
     S = HSV(:, :, 2);
-    if (kelas == 1 || kelas == 2 )
-        bw = im2bw(S, 0.3);
-    elseif (kelas == 3 || kelas == 6)
-        bw = im2bw(S, 0.2);
-    elseif (kelas == 4)
-        bw = im2bw(S, 0.4);
-    else
-        bw = im2bw(S, 0.5);
-    end
+    level = graythresh(S);
+    bw = imbinarize(S, level);
     bw = bwareaopen(bw, 1000);
     mask = strel('disk', 10);
     bw = imdilate(bw, mask);
@@ -48,5 +45,6 @@ function RGB = segmentasi(I, kelas)
     R(~bw) = 0; % mengubah nilai komponen warna merah dari citra diluar seleksi citra biner menjadi hitam (0)
     G(~bw) = 0; % mengubah nilai komponen warna hijau dari citra diluar seleksi citra biner menjadi hitam (0)
     B(~bw) = 0; % mengubah nilai komponen warna biru dari citra diluar seleksi citra biner menjadi hitam (0)
+    gray = bw;
     RGB = cat(3, R, G, B); % menyimpan komponen warna merah, hijau, dan biru ke dalam variabel RGB
 end
---
+++ tes_klasifikasi.m
@@ -0,0 +1,20 @@
+clc; clear; close all;
+
+klasifikasi('fitur.csv');
+
+function klasifikasi(namafile)
+    csv = readcell(namafile);
+    X = cell2mat(csv(:, 1:5));
+    y = csv(:, end)';
+    y = categorical(y);
+    Z = X(1:90, :);
+    yTest = y(1:90)';
+    kelas = {'Burger', 'Croissants', 'Muffin', 'Pizza', 'RotiSobek', 'RotiTawar'}; % kelas klasifikasi jenis roti
+    t = templateSVM('Standardize', true, 'KernelFunction', 'gaussian'); % membuat template svm terstandarisasi yang menyimpan support vector untuk model ecoc
+    SVMModel = fitcecoc(X, y, 'Learners', t, 'ClassNames', kelas);
+    CompactSVMModel = compact(SVMModel); % mengurangi ukuran SVMModel
+    [labels,PostProbs] = predict(CompactSVMModel, Z); % melakukan pengujian model svm menggunakan data uji dari matriks Z
+    table(Z, labels, PostProbs(:, 2),'VariableNames',...
+        {'TrueLabels','PredictedLabels','PosClassPosterior'})
+    acc = sum(labels == yTest)./numel(yTest)
+end
